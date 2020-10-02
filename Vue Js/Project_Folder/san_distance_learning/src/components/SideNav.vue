1dc74787c4bc4b2cae2b3473b3d69d593170b99a
samoelmp <71791930+samoelmp@users.noreply.github.com>
2020-10-01 18:58:59 -0700

    1

--- Vue Js/Project_Folder/san_distance_learning/src/components/SideNav.vue
+++ Vue Js/Project_Folder/san_distance_learning/src/components/SideNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-nav vertical class="w-25" allign="left" justified>
-      <b-nav-item active>Dash Board</b-nav-item>
+      <b-nav-item active>DashBoard</b-nav-item>
       <b-nav-item active>Grades</b-nav-item>
       <b-nav-item active>Calander</b-nav-item>
 <!--      <b-nav-item disabled>Disabled</b-nav-item>-->
