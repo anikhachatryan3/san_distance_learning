00b3073e7327955010ca60caaf3b4781b760ef08
natkes <itompir5@gmail.com>
2021-03-23 20:54:34 -0700

    Math assignments

--- backend/resources/js/components/StudentMathClass.vue
+++ backend/resources/js/components/StudentMathClass.vue
@@ -23,25 +23,64 @@
             <vs-col w="9">
                 <div id="data">
                    <!-- GAME STARTS HERE-->
-                    <div v-if="showTeacher" id="Teacher side">
-                        <b-dropdown class="m-md-2" text="Choose an operation">
-                            <b-dropdown-item @click="setSign('+')">Addition</b-dropdown-item>
-                            <b-dropdown-item @click="setSign('-')">Subtraction</b-dropdown-item>
-                            <b-dropdown-item @click="setSign('*')">Multiplication</b-dropdown-item>
-                        </b-dropdown>
-                    </div>
-                    <div id="student side" v-else>
-                        <p> Solve the equation and press submit to check your answers</p>
-                        <p> {{num1}} {{sign}} {{num2}} =</p>
-                        <b-input v-model="result" type="number"></b-input>
+                    <h1>Assignments</h1>
+                    <div id="assignments" v-for="assign in assignments">
+                        <b-button v-b-toggle="'collapse'+assign.id"collapse-1 variant="primary">{{assign.name}}</b-button>
                         <br/>
-                        <b-button @click="correct = checkPair(num1,num2,sign)">Check answer</b-button>
-                        <div v-if="showResults">
-                            <p style="margin: 0; display: inline;">Your answer is</p>
-                            <p v-if="correct" style="margin: 0; display: inline;" align="right">correct</p>
-                            <p v-else style="margin: 0; display: inline;" align="right">incorrect</p>
-                        </div>
+                        <b-collapse :id="'collapse'+assign.id" class="mt-2">
+                            <div v-for="problem in assign.math_problems">
+                                <b-card>
+                                    <p class="card-text"> {{problem.num1}} {{problem.operator}} {{problem.num2}}= </p>
+                                    <b-input type="number"> </b-input>
+                                </b-card>
+                            </div>
+                        </b-collapse>
                     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    <!--                    <div v-if="showTeacher" id="Teacher side">-->
+<!--                        <b-dropdown class="m-md-2" text="Choose an operation">-->
+<!--                            <b-dropdown-item @click="setSign('+')">Addition</b-dropdown-item>-->
+<!--                            <b-dropdown-item @click="setSign('-')">Subtraction</b-dropdown-item>-->
+<!--                            <b-dropdown-item @click="setSign('*')">Multiplication</b-dropdown-item>-->
+<!--                        </b-dropdown>-->
+<!--                    </div>-->
+<!--                    <div id="student side" v-else>-->
+<!--                        <p> Solve the equation and press submit to check your answers</p>-->
+<!--                        <p> {{num1}} {{sign}} {{num2}} =</p>-->
+<!--                        <b-input v-model="result" type="number"></b-input>-->
+<!--                        <br/>-->
+<!--                        <b-button @click="correct = checkPair(num1,num2,sign)">Check answer</b-button>-->
+<!--                        <div v-if="showResults">-->
+<!--                            <p style="margin: 0; display: inline;">Your answer is</p>-->
+<!--                            <p v-if="correct" style="margin: 0; display: inline;" align="right">correct</p>-->
+<!--                            <p v-else style="margin: 0; display: inline;" align="right">incorrect</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
                     <!--                    GAME ENDS HERE-->
                 </div>
             </vs-col>
@@ -64,6 +103,7 @@ export default {
     },
     data() {
         return {
+            course_id: this.$route.params.classId,
             assignment: null,
             result: null,
             num1: Math.floor(Math.random()*100)+1,
@@ -72,12 +112,15 @@ export default {
             showResults: false,
             showTeacher: true,
             sign: '',
+            assignments: [],
 
         }
     },
-    created:{
-        num1: Math.floor(Math.random()*100)+1,
-        num2: Math.floor(Math.random()*100)+1,
+    created(){
+        let self = this;
+        axios.get('/api/courses/'+ this.course_id).then(function (response) {
+            self.assignments = response.data.data.assignments
+        },);
     },
     methods:{
         checkPair(num1, num2, sign) {
--- backend/resources/js/components/TeacherMathClass.vue
+++ backend/resources/js/components/TeacherMathClass.vue
@@ -24,8 +24,9 @@
                 <div id="data">
                     <h1>Assignments</h1>
                     <div id="assignments" v-for="assign in assignments">
-                        <b-button v-b-toggle.collapse-1 variant="primary">{{assign.name}}</b-button>
-                        <b-collapse id="collapse-1" class="mt-2">
+                        <b-button v-b-toggle="'collapse'+assign.id"collapse-1 variant="primary">{{assign.name}}</b-button>
+                        <br/>
+                        <b-collapse :id="'collapse'+assign.id" class="mt-2">
                             <b-card>
                                 <div v-for="problem in assign.math_problems">
                                     <p class="card-text"> {{problem.num1}} {{problem.operator}} {{problem.num2}}= </p>
@@ -39,17 +40,16 @@
                     <div id="add assignment" class="col-md-3 col-md-offset-2" v-if="showCreate">
                         <h5>Assignment info:</h5>
                         <p>Assignment name:</p>
-                        <b-input v-model="this.assignment.name" type="text"></b-input>
+                        <b-input required v-model="assignment.name" type="text"></b-input>
                         <p>Number of exercises:</p>
-                        <b-input v-model="this.assignment.num_problems" type="number"></b-input>
-                        <b-radio v-model="this.assignment.operator" name="operator" value="+">Addition</b-radio>
-                        <b-radio v-model="this.assignment.operator" name="operator" value="-">Subtraction</b-radio>
-                        <b-radio v-model="this.assignment.operator" name="operator" value="*">Multiplication</b-radio>
+                        <b-input required v-model="assignment.num_problems" type="number"></b-input>
+                        <p>Assignment Type:</p>
+                        <b-select v-model="assignment.operator" :options="operator_options" required></b-select></b-radio>-->
                         <p>Min value:</p>
-                        <b-input v-model="this.assignment.range_min" type="number"></b-input>
+                        <b-input required v-model="assignment.range_min" type="number"></b-input>
                         <p>Max value:</p>
-                        <b-input v-model="this.assignment.range_max" type="number"></b-input>
-                        <vs-button active id="post assignment" @click="this.postAssign">Add assignment</vs-button>
+                        <b-input required v-model="assignment.range_max" type="number"></b-input>
+                        <vs-button active id="post assignment" @click="postAssign">Add assignment</vs-button>
                     </div>
 
                 </div>
@@ -77,12 +77,13 @@ export default {
             showCreate: false,
             course_id: this.$route.params.classId,
             assignment:{
-                name: null,
-                range_min: null,
-                operator: null,
-                range_max: null,
-                num_problems: null,
+                name: "",
+                range_min: "",
+                operator: "",
+                range_max: "",
+                num_problems: "",
             },
+            operator_options: [{ text: 'Choose...', value: null }, { text: 'Addition', value: '+' }, { text: 'Subtraction', value: '-' }, { text: 'Multiplication', value: '*' }],
 
             assignments: []
         }
@@ -94,14 +95,44 @@ export default {
         },);
     },
     methods:{
-        postAssign(){
+        onSubmit(){
             this.showCreate = false;
-        //    MAKE POST OP
-            this.assignment.name=null;
-            this.assignment.numberOfEX=null;
-            this.assignment.op=null;
-            this.assignment.min=null;
-            this.assignment.max=null;
+            this.postAssign().then(done=> console.log(done));
+            this.assignment.name="";
+            this.assignment.range_min="";
+            this.assignment.operator="";
+            this.assignment.range_max="";
+            this.assignment.num_problems="";
+        },
+        async postAssign(){
+            let timer = 0;
+            let done = false;
+            this.showCreate = false;
+            await axios.post('/api/courses/' + this.course_id + '/math-assignment',this.assignment).then(function (response){
+                done=true;
+            });
+            while(!done)
+            {
+                timer++;
+            }
+            console.log(timer);
+            let self = this;
+            axios.get('/api/courses/'+ this.course_id).then(function (response) {
+                self.assignments = response.data.data.assignments
+            },);
+            this.assignment.name="";
+            this.assignment.range_min="";
+            this.assignment.operator="";
+            this.assignment.range_max="";
+            this.assignment.num_problems="";
+        },
+        onReset(){
+            this.showCreate = false;
+            this.assignment.name="";
+            this.assignment.range_min="";
+            this.assignment.operator="";
+            this.assignment.range_max="";
+            this.assignment.num_problems="";
         },
         show(){
             this.showCreate=true;
