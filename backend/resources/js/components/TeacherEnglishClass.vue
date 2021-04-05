0dfa39808c05d9690d83139ef90ab60929b60e74
samoelmp <71791930+samoelmp@users.noreply.github.com>
2021-04-05 09:13:52 -0700

    TeacherEnglishClass

--- backend/resources/js/components/TeacherEnglishClass.vue
+++ backend/resources/js/components/TeacherEnglishClass.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="profile">
+    <div id="teacherEnglishClass">
         <Header />
         <NavBar />
         <br/>
@@ -23,9 +23,39 @@
             <vs-col w="10">
                 <div id="data">
                     <h1>Assignments</h1>
-                    <vs-button active id="create" to="TeacherEnglishClass/CreateEnglishAssignment">
-                        Create Assignment
+                    <div id="assignments" v-for="assign in assignments">
+                        <b-button v-b-toggle="'collapse'+assign.id" collapse-1 variant="primary">{{assign.name}}</b-button>
+                        <br/>
+                        <b-collapse :id="'collapse'+assign.id" class="mt-2">
+                            <b-card>
+                                <div v-for="problem in assign.english_problems">
+                                    <p class="card-text"> {{problem.word}}= </p>
+                                </div>
+                            </b-card>
+                        </b-collapse>
+                    </div>
+                    <vs-button active id="create" @click="show">
+                        Add Assignment
                     </vs-button>
+                    <br/>
+                    <div id="add assignment" class="col-md-3 col-md-offset-2" v-if="showCreate">
+                        <h5>Create an assignment:</h5>
+                        <table>
+                            <tr>
+                                <th><p>Assignment name: </p></th>
+                                <th><b-input required v-model="assignment.name" type="text"></b-input></th>
+                            </tr>
+                            <tr>
+                                <th><p>Number of exercises:</p></th>
+                                <th><b-input required v-model="assignment.num_problems" type="number"></b-input></th>
+                            </tr>
+                            <tr>
+                                <th><p>Assignment Type:</p></th>
+                                <th><b-select v-model="assignment.wordscramble" :options="scramble_options" required></b-select></th>
+                            </tr>
+                        </table>
+                        <vs-button active id="post assignment" @click="postAssign">Add assignment</vs-button>
+                    </div>
                 </div>
             </vs-col>
         </vs-row>
@@ -37,6 +67,7 @@
 import Header from "./Header.vue";
 import NavBar from "./NavBar.vue";
 import SideNav from "./SideNav.vue";
+import axios from "axios";
 
 export default {
     name: "TeacherEnglishClass",
@@ -44,6 +75,25 @@ export default {
         Header,
         NavBar,
         SideNav
+    },
+    data(){
+        return {
+            showCreate: false,
+            pop: false,
+            course_id: this.$route.params.classId,
+            assignment:{
+                name:"",
+                num_problems: "",
+                wordscramble: "",
+            },
+        assignments: [],
+        }
+    },
+    created(){
+        let self = this;
+        axios.get('/api/courses/'+ this.course_id).then(function (response) {
+            self.assignments = response.data.data.assignments
+        },);
     }
 };
 </script>
